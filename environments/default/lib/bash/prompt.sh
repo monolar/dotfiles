31e4388fd4e208632e5d2f177a62b735c2f60dbc
Andreas Marr <andreasm@artcom.de>
2014-01-08 13:17:19 +0100

    starting powerline manually for now.

--- environments/default/lib/bash/prompt.sh
+++ environments/default/lib/bash/prompt.sh
@@ -8,3 +8,7 @@ last_state() {
 }
 
 export PS1='\[\e[02;32m\]\u@\h:\[\e[0m\]\[\e[01;36m\]\W\[\e[0m\] $(last_state) \[\e[02;33m\][$(current_rvm)]\[\e[0m\]\[\e[01;35m\]$(git_prompt)\[\e[0m\] \[\e[01;33m\]$\[\e[0m\] '
+
+powerline_start() {
+  . /usr/local/lib/python2.7/site-packages/powerline/bindings/bash/powerline.sh
+}
