51e033466669cd7ebe085830d48d1ccbb4e4ec2b
Andreas Marr <andreasm@artcom.de>
2014-05-06 11:36:56 +0200

    added a convenience bash function for updating nimrod from git HEAD by compiling it from scratch.

--- environments/default/lib/nimrod/env.sh
+++ environments/default/lib/nimrod/env.sh
@@ -3,3 +3,17 @@ PATH="/opt/nimrod/bin:$PATH"
 
 # Babel package manager
 PATH="~/.babel/bin:$PATH"
+
+update_nimrod() {
+  echo "updating nimrod..."
+  cd ~/git/Nimrod
+  git pull
+  cd csources
+  git pull
+  sh build.sh
+  cd ..
+  bin/nimrod c koch
+  ./koch boot -d:release
+  echo "done building nimrod"
+  echo "install via 'sudo ./koch install /opt'"
+}
