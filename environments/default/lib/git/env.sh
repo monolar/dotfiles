465006c5d79a1653f5b63ba33b15d22c0036df6d
Andreas Marr <andreasm@artcom.de>
2014-06-28 16:25:53 +0200

    Git prompt now properly displays branches with '/' in their name.
    Supports only one '/' in the branch name - deeper nesting is anyway not recommended according to git-flow.
    
    This change displays a branche named 'feature/foo_bar' properly instead of just 'feature' which was not really helpful.

--- environments/default/lib/git/env.sh
+++ environments/default/lib/git/env.sh
@@ -18,7 +18,7 @@ function parse_git_stash {
 
 # Get the current git branch name (if available)
 git_prompt() {
-    local ref=$(git symbolic-ref HEAD 2>/dev/null | cut -d'/' -f3)
+    local ref=$(git symbolic-ref HEAD 2>/dev/null | cut -d'/' -f3,4)
     if [ "${ref}" != "" ]
     then
         echo -e " ($ref$(parse_git_dirty)$(parse_git_stash))"
