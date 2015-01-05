c2b8d0cdfc670d8a0a1bd4998ecc0a831789ff1a
Andreas Marr <andreasm@artcom.de>
2015-01-05 13:23:48 +0100

    better nimrod update process
    Now also allows specifying tag name (unchecked)

--- environments/default/lib/nim/env.sh
+++ environments/default/lib/nim/env.sh
@@ -1,19 +1,26 @@
 # Nimrod itself
-PATH="/opt/nim/bin:$PATH"
+PATH=/opt/nim/bin:$PATH
 
-# Babel package manager
-PATH="~/.nimble/bin:$PATH"
+# Nimble package manager
+PATH=~/.nimble/bin:$PATH
 
 update_nimrod() {
   echo "updating nim..."
   cd ~/git/Nim
+  if [ -d "csources" ]
+  then
+    rm -rf csources
+  fi
+
   git pull
-  cd csources
-  git pull
+  if [ "$1" ]
+  then
+    echo "switching to tag: ${1}"
+    git checkout ${1}
+  fi
+
   sh build.sh
-  cd ..
-  bin/nim c koch
-  ./koch boot -d:release
+
   echo "done building nim"
   echo "install via 'sudo ./koch install /opt'"
 }
