f7ceda6807765302ec70df0976504a6cc16d552e
Andreas Marr <monolar@monolar.org>
2015-10-09 13:58:07 +0200

    Additional os x defaults

--- environments/default/os_x.sh
+++ environments/default/os_x.sh
@@ -3,16 +3,32 @@
 PROG=${0##*/}
 
 # OS X Yosemite+ only...
-if ! [[ $(sw_vers -productVersion) =~ ^10\.1.\..+ ]]; then
-  echo "$PROG script is intended only for OS X versions 10.10.0 and up"
-  exit 1
-fi
+#if ! [[ $(sw_vers -productVersion) =~ ^10\.11.\..+ ]]; then
+#  echo "$PROG script is intended only for OS X versions 10.10.0 and up"
+#  exit 1
+#fi
 
 # Configuring OS X Settings
 echo "Configuring OS X"
 
-# Ask for the administrator password upfront.
+# Ask for the administrator password upfront
 sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+## General
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+
+# Set computer name
+sudo scutil --set ComputerName "M"
+sudo scutil --set HostName "M"
+sudo scutil --set LocalHostName "M"
+
+# Set standby to 24h
+sudo pmset -a standbydelay 86400
 
 ## UI
 
@@ -35,7 +51,15 @@ defaults write com.apple.universalaccess reduceTransparency -boolean true
 # Disable shadows on screen captures (default: not set).
 defaults write com.apple.screencapture disable-shadow -boolean true
 
-## Finder
+##################################
+## Mouse / Keyboard / Scrolling ##
+##################################
+
+# Disable “natural” (Lion-style) scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+############
+## Finder ##
 
 # New windows show home directory.
 defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
@@ -51,5 +75,13 @@ defaults write com.apple.finder ShowRemovableMediaOnDesktop -boolean true
 
 # Show status bar.
 defaults write com.apple.finder ShowStatusBar -boolean true
+
+## Misc Settings
+
+# Disable smart quotes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+
+# Disable smart dashes as they’re annoying when typing code
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 
 echo "You may need to at least log out and log in again for changes to take effect!"
