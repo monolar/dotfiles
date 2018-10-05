be2d3fc5627e06cc852321535c92bd1085c3701a
Andreas Marr <monolar@monolar.org>
2018-10-05 11:06:09 +0200

    Update to Mojave
    Also general cleanup. This was not committed for a while and i lost changes so there might still be things missing, but overall this at least works again.

--- environments/default/brew.sh
+++ environments/default/brew.sh
@@ -4,7 +4,7 @@
 # TODO: Be more defensively
 
 function _get_root_permissions() {
-  echo "getting root permissions once at the start..."
+  echo "* getting root permissions once at the start..."
   # Ask for the administrator password upfront.
   sudo -v
 
@@ -14,28 +14,26 @@ function _get_root_permissions() {
 
 _get_root_permissions
 
-# XCode sdk
-xcode-select --install
+which -s brew
+if [[ $? != 0 ]] ; then
+    echo "* install homebrew ..."
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+else
+    echo "* update homebrew ..."
+    brew update
+fi
 
-# Install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-echo "checking brew..."
+echo "* checking homebrew ..."
 brew doctor
 
 function _taps() {
   taps=(
-    codeclimate/formulae
-    caskroom/cask
-    caskroom/versions
-    homebrew/dupes
-    homebrew/versions
-    manastech/crystal
+    # codeclimate/formulae
     martido/brew-graph
-    homebrew/science
-    homebrew/python
-    adymo/kde
-    thoughtbot/formulae
+    # homebrew/science
+    # homebrew/python
+    # adymo/kde
+    # thoughtbot/formulae
   )
   for i in "${taps[@]}"
   do
@@ -47,23 +45,23 @@ function _taps() {
 
 _taps
 
-# Make sure we’re using the latest Homebrew.
-#echo "updating brews..."
-#brew update
-
-# Upgrade any already-installed formulae.
-#echo "upgrading brews..."
-#brew upgrade
+echo "Do you wish to upgrade all packages?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) echo "* Upgrade all packages"; brew upgrade; break;;
+        No ) echo "* Continue without upgrading packages"; break;;
+    esac
+done
 
 # All "normal" brew packages...
-echo "... brewing ..."
+echo "* brewing ..."
 brew install brew-graph
 brew install bash fish
 brew install bash-completion2
 brew install coreutils
 brew install binutils
-brew install git --with-brewed-curl --with-brewed-openssl --with-brewed-svn --with-gettext
-brew install gitsh
+brew install git --with-curl --with-openssl
+brew install git-sh
 brew install grep --with-default-names
 brew install z
 brew install sqlite
@@ -76,20 +74,32 @@ brew install reattach-to-user-namespace
 brew install cmake --with-completion
 brew install python
 brew install vim --override-system-vi
-brew install tmux ranger htop wget
+brew install tmux
+brew install ranger
+brew install htop
+brew install pstree
 brew install the_silver_searcher # faster search
 brew install fzf # command line fuzzy finder
-brew install ctags stow
-brew install mysql node tree tig
-brew install crystal-lang
+brew install ctags
+brew install stow
+brew install mysql
+brew install node
+brew install tree
+brew install tig
+brew install crystal
+brew install rust
 brew install graphviz --with-librsvg --with-x11
-brew install nginx httpie
-brew install go haskell-stack nimrod
+brew install nginx
+brew install httpie
+brew install wget
+brew install go
+brew install haskell-stack nimrod
 brew install zopfli # compression algorithm
-brew install watch thefuck
+brew install watch
+brew install thefuck
 brew install the_platinum_searcher
-brew install since pup pstree
-brew install neo4j # Graph Database
+brew install since
+brew install pup
 brew install mercurial # Source code versioning system
 brew install v8 haproxy midnight-commander
 brew install logtalk # prolog inspired programming language
@@ -102,78 +112,83 @@ brew install pv # Pipeline
 
 # install PIL, imagemagick, graphviz and other
 # image generating stuff
-brew install libtiff libjpeg webp little-cms2
+brew install libtiff
+brew install libjpeg
+brew install webp
+brew install little-cms2
 
+brew cask install xquartz
 brew install imagemagick --with-fftw --with-librsvg --with-x11
 brew install cairo
-
-brew install Caskroom/cask/intellij-idea
-
-# Install brew cask
-brew install caskroom/cask/brew-cask
-brew install Caskroom/cask/xquartz
 
 brew install py2cairo # this will ask you to download xquartz and install it
 brew install pyqt
 
-brew install adymo/kde/massif-visualizer # for memory profile visualisation
+# brew install adymo/kde/massif-visualizer # for memory profile visualisation
 brew install watchman # watch fs changes
 brew install memcache-top
 
 # Docker
-brew install docker-machine
-brew install codeclimate
+# brew install docker-machine
+# brew install codeclimate
 brew install docker
 brew install docker-compose
 
-# Install cask applications
 echo "installing Cask Applications..."
 
-brew install Caskroom/cask/easysimbl
-echo "Starting EasySIMBL: Check 'Use SIMBL' checkbox..."
-open ~/Applications/EasySIMBL.app
+# Java8 and neo4j
+brew cask install java8
+brew install neo4j # Graph Database
 
-brew install Caskroom/cask/iterm2 # Terminal alternative
-brew install Caskroom/cask/mou # Markdown Editor
-brew install Caskroom/cask/google-chrome # Browser
-brew install Caskroom/cask/slack # Messaging
-brew install Caskroom/cask/adium # Chat client
-brew install Caskroom/cask/java
+brew cask install intellij-idea
+brew cask install pycharm
+
+# brew install Caskroom/cask/easysimbl
+# echo "Starting EasySIMBL: Check 'Use SIMBL' checkbox..."
+# open ~/Applications/EasySIMBL.app
+
+brew cask install iterm2 # Terminal alternative
+# brew cask install mou # Markdown Editor
+# brew cask install google-chrome # Browser
+brew cask install slack # Messaging
+brew cask install adium # Chat client
+brew cask install java
 brew install gradle # Java build tool like maven
-brew install Caskroom/cask/mysqlworkbench
-brew install Caskroom/cask/github-desktop # Github ui client
-brew install Caskroom/cask/sequel-pro # Sql workbench
-brew install Caskroom/cask/atom # Editor
-brew install Caskroom/cask/jewelrybox # RVM UI
-brew install Caskroom/cask/cocoarestclient
-brew install Caskroom/cask/macdown # Another Markdown editor
-brew install Caskroom/cask/skype
-brew install Caskroom/cask/virtualbox
-brew install Caskroom/cask/imageoptim # Image Optimizer
-brew install Caskroom/cask/licecap # Record animated gifs
-brew install Caskroom/cask/gimp # image manipulation
-brew install Caskroom/cask/onyx # OS X maintenance and power tools
-brew install Caskroom/cask/secrets # OS X settings panel
-brew install Caskroom/cask/firefox
-brew install Caskroom/cask/spectacle # manage windows and their positions
-brew install Caskroom/cask/cakebrew # Brew UI client
-brew install Caskroom/cask/fontforge
-brew install Caskroom/cask/blender # i like modelling and do papercraft stuff.
-brew install Caskroom/cask/steam # i am a gamer
-brew install Caskroom/cask/vlc
+brew cask install mysqlworkbench
+brew cask install github # Github ui client
+brew cask install sequel-pro # Sql workbench
+brew cask install atom # Editor
+brew cask install visual-studio-code
+brew cask install jewelrybox # RVM UI
+brew cask install cocoarestclient
+brew cask install macdown # Another Markdown editor
+brew cask install skype
+brew cask install virtualbox
+brew cask install imageoptim # Image Optimizer
+brew cask install licecap # Record animated gifs
+brew cask install gimp # image manipulation
+brew cask install onyx # OS X maintenance and power tools
+# brew cask install secrets # OS X settings panel
+brew cask install firefox
+brew cask install spectacle # manage windows and their positions
+brew cask install cakebrew # Brew UI client
+brew cask install fontforge
+brew cask install blender # i like modelling and do papercraft stuff.
+brew cask install steam # i am a gamer
+brew cask install vlc
 ## Commercial Cask Applications (may work as trial)
-brew install Caskroom/versions/sublime-text3
-brew install Caskroom/cask/tower # installs tower2
-brew install Caskroom/cask/libreoffice # productivity stuff you cannot without
-brew install Caskroom/cask/kitematic # Docker
-brew install Caskroom/cask/disk-inventory-x # file system overview
-brew install Caskroom/cask/dbvisualizer
-brew install Caskroom/cask/sourcetree # another git client
-brew install Caskroom/cask/rdm # redis ui client
-brew install Caskroom/cask/clipy # A Clipboard extension
+# brew cask install sublime-text3
+# brew cask install tower # installs tower2
+brew cask install libreoffice # productivity stuff you cannot without
+# brew install Caskroom/cask/kitematic # Docker
+brew cask install disk-inventory-x # file system overview
+brew cask install dbvisualizer
+# brew cask install sourcetree # another git client
+# brew cask install rdm # redis ui client
+brew cask install clipy # A Clipboard extension
 
 # Quicklook plugins
-brew install Caskroom/cask/qlmarkdown # markdown
+brew cask install qlmarkdown # markdown
 
 echo "Cleaning Brews..."
 brew cleanup
