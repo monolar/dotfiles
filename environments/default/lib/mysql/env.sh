4cd79810d64d1c0c25be6056f8fdf49884d85583
Andreas Marr <monolar@monolar.org>
2018-10-23 13:09:49 +0200

    Switch to mysql 5.7

--- environments/default/lib/mysql/env.sh
+++ environments/default/lib/mysql/env.sh
@@ -1,6 +1,12 @@
-# For mysql-client package
-PATH="/usr/local/opt/mysql-client/bin:$PATH"
+# MYSQL 8
+# # For mysql-client package
+# PATH="/usr/local/opt/mysql-client/bin:$PATH"
+# export LDFLAGS="-L/usr/local/opt/mysql-client/lib"
+# export CPPFLAGS="-I/usr/local/opt/mysql-client/include"
+# export PKG_CONFIG_PATH="/usr/local/opt/mysql-client/lib/pkgconfig"
 
-export LDFLAGS="-L/usr/local/opt/mysql-client/lib"
-export CPPFLAGS="-I/usr/local/opt/mysql-client/include"
-export PKG_CONFIG_PATH="/usr/local/opt/mysql-client/lib/pkgconfig"
+# Mysql 5.7
+export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+export LDFLAGS="-L/usr/local/opt/mysql@5.7/lib"
+export CPPFLAGS="-I/usr/local/opt/mysql@5.7/include"
+export PKG_CONFIG_PATH="/usr/local/opt/mysql@5.7/lib/pkgconfig"
