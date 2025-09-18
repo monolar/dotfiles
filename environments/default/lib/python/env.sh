cc3d329f30a5d1ecefc6351a77d244fcb332c46e
Andreas Marr <andreas.marr@zattoo.com>
2025-09-18 12:25:19 +0200

    support multiple python env systems

--- environments/default/lib/python/env.sh
+++ environments/default/lib/python/env.sh
@@ -9,5 +9,34 @@ export PYENV_ROOT="$HOME/.pyenv"
 eval "$(pyenv init --path)"
 
 current_pyenv_virtualenv() {
-      echo -en `pyenv version-name`
+    # Check if we're in a .venv (most specific)
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        local venv_name=$(basename "$VIRTUAL_ENV")
+        local python_version=$(python --version 2>&1 | cut -d' ' -f2)
+        echo -en "venv:${venv_name}(${python_version})"
+        return
+    fi
+
+    # Check if we're in a conda environment
+    if [[ -n "$CONDA_DEFAULT_ENV" ]]; then
+        local python_version=$(python --version 2>&1 | cut -d' ' -f2)
+        echo -en "conda:${CONDA_DEFAULT_ENV}(${python_version})"
+        return
+    fi
+
+    # Check if we're in a pyenv-virtualenv
+    local pyenv_version=$(pyenv version-name 2>/dev/null)
+    if [[ -n "$pyenv_version" && "$pyenv_version" != "system" ]]; then
+        # If it contains a slash, it's likely a virtualenv
+        if [[ "$pyenv_version" == *"/"* ]]; then
+            local env_name=${pyenv_version##*/}  # Get part after last slash
+            echo -en "pyenv:${env_name}"
+        else
+            echo -en "pyenv:${pyenv_version}"
+        fi
+        return
+    fi
+
+    # Fallback to system Python
+    echo -en "sys:$(python --version 2>&1 | cut -d' ' -f2)"
 }
