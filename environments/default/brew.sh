768f9d6afca95c6b3faa60673900bf50c3759c28
Andreas Marr <monolar@monolar.org>
2026-01-21 15:18:30 +0100

    Replace RVM install with rbenv

--- environments/default/brew.sh
+++ environments/default/brew.sh
@@ -241,6 +241,8 @@ brew install --cask quicklook-json
 echo "Cleaning Brews..."
 brew cleanup
 
-# Install RVM
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable
+# Install Ruby tooling with rbenv (avoids remote script execution)
+brew install rbenv ruby-build
+if command -v rbenv >/dev/null; then
+  eval "$(rbenv init -)"
+fi
