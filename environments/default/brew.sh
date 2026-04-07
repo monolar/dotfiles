1ab5c0dcf6de91f81966fa363fcc8bba10ffb78c
Andreas Marr <monolar@monolar.org>
2026-04-07 17:38:53 +0200

    recategorize packages

--- environments/default/brew.sh
+++ environments/default/brew.sh
@@ -53,165 +53,196 @@ select yn in "Yes" "No"; do
     esac
 done
 
-# All "normal" brew packages...
-echo "* brewing ..."
+# Formulae
+echo "* brewing formulae ..."
+
+# Shells and core CLI
 brew install bash fish
 brew install bash-completion2
 brew install coreutils
 brew install binutils
-brew install git
-brew install subversion
 brew install grep
-brew install z
-brew install sqlite
-brew install gpg
-brew install grc
 brew install findutils
 brew install gnu-tar
 brew install gawk
+brew install z
+brew install grc
 brew install reattach-to-user-namespace
-brew install cmake
-brew install python
-brew install pyenv
-brew install pyenv-virtualenv
-brew install --cask google-cloud-sdk
-brew install pipenv
+brew install tree
+brew install watch
+brew install pick # for picking stuff from big lists
+brew install pv # Pipeline
+
+# Editors and terminal workflow
 brew install vim --override-system-vi
 brew install vimpager
 brew install neovim
 brew install tmux
 brew install ranger
+brew install midnight-commander
+brew install ghostty # a nice terminal emulator
+
+# Search, navigation, and inspection
 brew install htop
 brew install pstree
 brew install the_silver_searcher # faster search
+brew install ripgrep # rust powered grep/ag alternative
 brew install fd # find alternative (gitignore aware)
 brew install fzf # command line fuzzy finder
 brew install ctags
-brew install stow
-brew install mysql@8.0
-brew install mysql-client
-brew install node
-brew install tree
-brew install tig
-brew install crystal
-brew install rust
-brew install graphviz
-brew install nginx
-brew install httpie
-brew install wget
-brew install go
-brew install stack nim
-brew install zopfli # compression algorithm
-brew install watch
-# brew install thefuck
-# brew install the_platinum_searcher
-brew install since
-brew install mercurial # Source code versioning system
-brew install v8 haproxy midnight-commander
-brew install logtalk # prolog inspired programming language
-brew install qcachegrind # for profile data visualisation
-brew install qt
-brew install gifsicle # for optimizing GIFs
-brew install pick # for picking stuff from big lists
 brew install cloc # source code line counter
-brew install pv # Pipeline
+brew install bat # cat for markdown
 brew install jq # Command line json processor
 brew install jsonpp # Command line json pretty printer
+brew install csvkit # nice tools for csv operations
+
+# Version control and dotfiles
+brew install git
+brew install subversion
+brew install mercurial # Source code versioning system
+brew install tig
+brew install stow
+
+# Languages and build tooling
+brew install cmake
+brew install python
+brew install pyenv
+brew install pyenv-virtualenv
+brew install pipenv
+brew install node
+brew install go
+brew install rust
+brew install crystal
+brew install nim
+brew install stack
+brew install rbenv ruby-build
+brew install qt
+brew install pyqt
+brew install v8
 brew install zlib
+brew install zig
+
+# Networking, HTTP, and infrastructure
+brew install httpie
+brew install wget
+brew install nginx
+brew install haproxy
+brew install watchman # watch fs changes
+brew install watchexec # watch fs changes (gitignore aware)
+brew install docker-compose
+
+# Databases and services
+brew install sqlite
+brew install mysql@8.0
+brew install mysql-client
 brew install redis
 brew install memcached
-brew install ripgrep # rust powered grep/ag alternative
-brew install lcov
-brew install csvkit # nice tools for csv operations
-brew install bat # cat for markdown
-brew install ghostty # a nice terminal emulator
+brew install memcache-top
+brew install neo4j # Graph Database
 
-# install PIL, imagemagick, graphviz and other
-# image generating stuff
+# Security and certificates
+brew install gpg
+
+# Data, docs, and utilities
+brew install graphviz
+brew install qcachegrind # for profile data visualisation
+brew install since
+brew install logtalk # prolog inspired programming language
+brew install zopfli # compression algorithm
+brew install lcov
+
+# Imaging and graphics
 brew install libtiff
 brew install libjpeg
 brew install webp
 brew install little-cms2
-
-brew install --cask xquartz
 brew install imagemagick
 brew install cairo
+brew install gifsicle # for optimizing GIFs
 
-brew install pyqt
-
-# brew install adymo/kde/massif-visualizer # for memory profile visualisation
-brew install watchman # watch fs changes
-brew install watchexec # watch fs changes (gitignore aware)
-brew install memcache-top
-
-# Java and neo4j
-brew install neo4j # Graph Database
-
+# Casks
 echo "installing Cask Applications..."
-brew install --cask docker
 
+# Cloud and containers
+brew install --cask google-cloud-sdk
+brew install --cask docker
+brew install --cask virtualbox
+brew install --cask tunnelblick # A VPN Client
+brew install --cask vyprvpn
+
+# Developer IDEs and editors
 brew install --cask intellij-idea-ce
 brew install --cask pycharm-ce
+brew install --cask visual-studio-code
+brew install --cask github # Github ui client
+brew install --cask tower # tower2 git ui client
+brew install --cask sourcetree # another git client
+brew install --cask gitkraken # some git ui
+brew install --cask versions # svn ui client
 
-brew install --cask iterm2 # Terminal alternative
-brew install --cask slack # Messaging
-brew install --cask adium # Chat client
+# Java and API tooling
 brew install --cask temurin
 brew install gradle # Java build tool like maven
-brew install --cask mysqlworkbench
-brew install --cask github # Github ui client
-brew install --cask sequel-ace # Sql workbench
-brew install --cask tableplus # another sql workbench (also supports REDIS!!)
-brew install --cask datagrip # Jetbrains' take on sql workbench
-brew install --cask visual-studio-code
-brew install --cask jewelrybox # RVM UI
-brew install --cask cocoarestclient
-brew install --cask macdown # Another Markdown editor
-brew install --cask skype
-brew install --cask virtualbox
-brew install --cask imageoptim # Image Optimizer
-brew install --cask licecap # Record animated gifs
-brew install --cask gimp # image manipulation
-brew install --cask onyx # OS X maintenance and power tools
-# brew install --cask secrets # OS X settings panel
-brew install --cask firefox
-brew install --cask fontforge
-brew install --cask blender # i like modelling and do papercraft stuff.
-brew install --cask steam # i am a gamer
-brew install --cask vlc
-## Commercial Cask Applications (may work as trial)
-brew install --cask tower # tower2 git ui client
-brew install --cask versions # svn ui client
-brew install --cask libreoffice
-brew install --cask disk-inventory-x # file system overview
-brew install --cask dbvisualizer
-brew install --cask sourcetree # another git client
-brew install --cask clipy # A Clipboard extension
-brew install --cask tunnelblick # A VPN Client
 brew install --cask postman
-brew install --cask servpane # convenient way to start/stop brew services from menubar
-brew install --cask inkscape
-brew install --cask fantastical # iCal alternative
-brew install --cask gitkraken # some git ui
-brew install --cask deltawalker # diff/merge ui tool
-brew install --cask beyond-compare # diff/merge ui tool
-brew install --cask kaleidoscope # diff/merge ui tool
 brew install --cask insomnia # REST ui Testing tool
 brew install --cask soapui # REST ui Testing workbench
 brew install --cask paw # api tool
-brew install --cask vyprvpn
-brew install --cask whatsapp # yeah, i know
-brew install --cask alacritty # gpu based terminal emulator
+brew install --cask cocoarestclient
 
-# Quicklook plugins
+# Database clients
+brew install --cask mysqlworkbench
+brew install --cask sequel-ace # Sql workbench
+brew install --cask tableplus # another sql workbench (also supports REDIS!!)
+brew install --cask datagrip # Jetbrains' take on sql workbench
+brew install --cask dbvisualizer
+
+# Terminal and shell utilities
+brew install --cask iterm2 # Terminal alternative
+brew install --cask alacritty # gpu based terminal emulator
+brew install --cask servpane # convenient way to start/stop brew services from menubar
+brew install --cask clipy # A Clipboard extension
+
+# Communication and collaboration
+brew install --cask slack # Messaging
+brew install --cask adium # Chat client
+brew install --cask skype
+brew install --cask whatsapp # yeah, i know
+
+# Browsers and productivity
+brew install --cask firefox
+brew install --cask libreoffice
+brew install --cask fantastical # iCal alternative
+brew install --cask macdown # Another Markdown editor
+
+# Media, design, and graphics
+brew install --cask xquartz
+brew install --cask imageoptim # Image Optimizer
+brew install --cask licecap # Record animated gifs
+brew install --cask gimp # image manipulation
+brew install --cask inkscape
+brew install --cask fontforge
+brew install --cask blender # i like modelling and do papercraft stuff.
+brew install --cask vlc
+
+# System utilities and inspection
+brew install --cask onyx # OS X maintenance and power tools
+brew install --cask disk-inventory-x # file system overview
+brew install --cask jewelrybox # RVM UI
+
+# Commercial or optional desktop apps
+brew install --cask beyond-compare # diff/merge ui tool
+brew install --cask deltawalker # diff/merge ui tool
+brew install --cask kaleidoscope # diff/merge ui tool
+brew install --cask steam # i am a gamer
+
+# Quick Look plugins
 brew install --cask qlmarkdown # markdown
 brew install --cask quicklook-json
 
 echo "Cleaning Brews..."
 brew cleanup
 
-# Install Ruby tooling with rbenv (avoids remote script execution)
-brew install rbenv ruby-build
+# Initialize rbenv after installation
 if command -v rbenv >/dev/null; then
   eval "$(rbenv init -)"
 fi
